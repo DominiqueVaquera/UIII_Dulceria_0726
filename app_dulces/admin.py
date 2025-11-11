84a5d8ccbbc2866bccebe58cdab5436861f39130
DominiqueVaquera <vaqueradominique@gmail.com>
2025-11-11 13:17:25 -0600

    Delete app_dulces directory

--- app_dulces/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Dulce
-
-@admin.register(Dulce)
-class DulceAdmin(admin.ModelAdmin):
-    list_display = ('nombre', 'precio', 'stock', 'categoria', 'fecha_disponibilidad')
-    list_filter = ('categoria',)
-    search_fields = ('nombre', 'descripcion')
